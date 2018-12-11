7ee0a8c5504ff88b83ae33d54f9f6b1fed384db0
Daniel Burke <danieljburke@me.com>
2018-12-11 22:14:43 +1100

    magic whoosh api

--- guy.js
+++ guy.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const { parse } = require('querystring');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -8,28 +9,46 @@ var dots = require('dot').process({path: "."});
 
 var items = [];
 
+function add(item) {
+	items.push(item);
+}
+
+function deleteItem(item) {
+	var index = items.indexOf(item);
+	if (index == -1)
+		return;
+	items.splice(index, 1);
+}
+
+function move(item, position) {
+	deleteItem(item);
+	items.splice(position, 0, item);
+}
+
 const server = http.createServer((req, res) => {
 
-	if (req.method == 'POST')
+	var requestURL = url.parse(req.url, true);
+	switch (String(requestURL.pathname))
 	{
-		let body = ' ';
-		req.on('data', chunk => {
-			body += chunk.toString();
-		});
-		req.on('end', () => {
-			items.push(parse(body)[' item'].toString());
-		});
-		res.writeHead(302, {
-			'Location': '/'
-		});
-		res.end();
+		case "/add":
+			console.log("add");
+			add(requestURL.query.item);
+			break;
+
+		case "/delete":
+			console.log("delete");
+			deleteItem(requestURL.query.item);
+			break;
+
+		case "/move":
+			console.log("move");
+			move(requestURL.query.item, 0);
+			break;
 	}
-	else
-	{
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		res.end(dots.template({list: items}));
-	}
+
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(dots.template({list: items}));
 });
 
 server.listen(port, hostname, () => {
